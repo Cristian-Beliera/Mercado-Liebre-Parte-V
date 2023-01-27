082a1583ddd1d39d8fc5ed0754971a7f09457929
Comision19 <cristianbeliera@gmail.com>
2023-01-27 20:48:09 -0300

    Crecacion de forms login y register

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const PORT = 3050;
 
 app.use(express.static('public'));
 
-app.use("/", (req,res) => {
+app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+})
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
 })
 
 app.listen(PORT, () =>
