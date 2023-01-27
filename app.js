b39d866a2ff5418bf059f19bf9dd7def250a209a
Comision19 <cristianbeliera@gmail.com>
2023-01-27 16:51:02 -0300

    First commit parte V de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3050;
+
+app.use(express.static('public'));
+
+app.use("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+
+app.listen(PORT, () =>
+    console.log(`Server listen in port ${PORT}\n http://localhost:${PORT}`));
